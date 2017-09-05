ce60046742fadc0eb2220d7f87b28ec7d043055e
cmehrabian <cameron.mehrabian@gmail.com>
2017-09-05 16:04:37 -0700

    adds base component scaffolding + ant design

---
+++ client/src/dummydata.js
@@ -0,0 +1,65 @@
+const dummydata = {
+  profiles: [
+    {
+      id: "1",
+      first: "Cameron",
+      last: "Mehrabian",
+      display: "cmehrabian",
+      email: "cameron.mehrabian+localized@gmail.com",
+      phone: "4157441234",
+      avatar: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAgxAAAAJDg3N2UwOTkzLWM4MDAtNDQ3Yi04YjNjLWVmODQwYmM1NmUwZg.jpg"
+    },
+    {
+      id: "2",
+      first: "Dad",
+      last: "",
+      display: "dad",
+      email: "cameron.mehrabian+dad@gmail.com",
+      phone: "4157442345",
+      avatar: "https://accrualnet.cancer.gov/sites/accrualnet.cancer.gov/themes/accrualnet/accrualnet-internals/images/avatars/male/Blue.png"
+    },
+    {
+      id: "3",
+      first: "Mom",
+      last: "",
+      display: "mom",
+      email: "cameron.mehrabian+mom@gmail.com",
+      phone: "4157443456",
+      avatar: "https://accrualnet.cancer.gov/sites/accrualnet.cancer.gov/themes/accrualnet/accrualnet-internals/images/avatars/male/Blue.png"
+    }
+  ],
+  posts: [
+    {
+      id: 10988680,
+      title: "Family Picture",
+      description: "Great time with the fam!",
+      url: "https://a0.muscache.com/im/pictures/2c6eb3ad-a639-4be1-a592-149af50d67fc.jpg?aki_policy=small",
+      type: "image",
+      created_at: "12/24/2016",
+      family_id: 1,
+      meta_id: 1
+    },
+    {
+      id: 10988681,
+      title: "Family Song",
+      description: "Great song with the fam!",
+      url: "https://a0.muscache.com/im/pictures/2c6eb3ad-a639-4be1-a592-149af50d67fc.jpg?aki_policy=small",
+      type: "audo",
+      created_at: "12/24/2016",
+      family_id: 1,
+      meta_id: 2
+    },
+    {
+      "id": 10988682,
+      "title": "Picture",
+      "description": "Great time with the fam!",
+      "url": "https://a0.muscache.com/im/pictures/2c6eb3ad-a639-4be1-a592-149af50d67fc.jpg?aki_policy=small",
+      "type": "image",
+      created_at: "12/24/2016",
+      family_id: 1,
+      meta_id: 3
+    }
+  ]
+};
+
+export default dummydata;
