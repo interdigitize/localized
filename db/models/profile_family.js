b9007cce83f29d3a6cf8f885fd8b70fd6565e7ad
Mykola <kolya182@gmail.com>
2017-09-06 12:41:03 -0700

    Initial model

---
+++ db/models/profile_family.js
@@ -0,0 +1,15 @@
+const db = require('../');
+const Profiles = require('./');
+const Families = require('./');
+
+const Profile_Families = db.Model.extend({
+  tableName: 'profile_families',
+  families_id: function(){
+    return this.hasmany(Profiles);
+  },
+  profiles_id: function() {
+    return this.hasMany(Profiles);
+  }
+});
+
+module.exports = db.model('Profile_Families', Profile_Families);
