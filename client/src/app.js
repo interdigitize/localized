25378cea0e383198114de7accdbb08d461b2f9bf
interdigitize <interdigitize@users.noreply.github.com>
2017-09-05 20:40:38 -0700

    Sets local language to enUS.

--- client/src/app.js
+++ client/src/app.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './Index.jsx';
+import { LocaleProvider } from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
 
 ReactDOM.render(
-  <Index />,
+  <LocaleProvider locale={enUS}>
+    <Index />
+  </LocaleProvider>,
   document.getElementById('root')
 );
