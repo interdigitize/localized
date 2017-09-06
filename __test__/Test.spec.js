be088e0e7f5c63eee06ebcbf480c8b78afa0c4b5
interdigitize <interdigitize@users.noreply.github.com>
2017-09-05 21:29:52 -0700

    Updates test spec

--- __test__/Test.spec.js
+++ __test__/Test.spec.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
 describe('Addition', () => {
   it('knows that 2 and 2 make 4', () => {
     expect(2 + 2).toBe(4);
   });
+});
+
+describe('<Upload />', () => {
+  it('should render one <Modal /> component', () => {
+    const wrapper = shallow(<Upload />);
+    expect(wrapper.find(Modal)).to.have.length(1);
+  });
+  // it('should render an `.icon-star`', () => {
+  //   const wrapper = shallow(<MyComponent />);
+  //   expect(wrapper.find('.icon-star')).to.have.length(1);
+  // });
 });
