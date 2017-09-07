8c615949b0ebf86dedb19a307b510a6af6fcb4b5
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-07 14:37:44 -0700

    Add div with styling to render each family member image

--- client/src/components/FamiliesContainer.jsx
+++ client/src/components/FamiliesContainer.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Family from './Family.jsx';
 import AddFamily from './AddFamily.jsx';
+import family from '../dummyfamilydata';
 
 const FamiliesContainer = (props) => (
-  <div>
-    <div><Family /></div>
-    <div style= {{float: 'right', padding: '20px'}}>
+  <div style = {{padding: '20px'}}>
+    My Family
+    <div style = {{padding: '10px', display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+      {props.familyImages.map((member, i) => (
+        <Family key={i} member={member}/>
+      ))}
+    </div>
+    <div style= {{float: 'right', padding: '10px'}}>
       <AddFamily />
     </div>
   </div>
