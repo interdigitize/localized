c0bf4e550afe43a742f4a92577d961ead272eeae
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-07 00:26:37 -0700

    Remove Main from render

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Navigation from './components/navigation.jsx';
 import dummydata from './dummydata';
-import Main from './components/Main.jsx';
 import Footer from './components/Footer.jsx';
 import 'antd/dist/antd.css';
 
@@ -9,12 +8,11 @@ class Index extends Component {
   constructor(props) {
     super(props);
   }
-  
+
   render() {
     return(
       <div>
         <Navigation />
-        <Main />
         <Footer />
       </div>
     );
