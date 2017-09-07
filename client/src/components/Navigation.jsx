bb522bce554fbefb3620cd09a3297beced713477
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-06 23:46:26 -0700

    Move expand/collapse button into menu

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -74,31 +74,31 @@ class Navigation extends React.Component {
           <div className="logo" />
           <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
             <Menu.Item key="1">
+              <Icon
+                className="trigger"
+                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                onClick={this.toggle}/>
+              <span>Expand/Collapse</span>
+            </Menu.Item>
+            <Menu.Item key="2">
               <Icon type="home" />
               <span>Home</span>
             </Menu.Item>
-            <Menu.Item key="2">
+            <Menu.Item key="3">
               <Icon type="user" />
               <span>Profile</span>
             </Menu.Item>
-            <Menu.Item key="3">
+            <Menu.Item key="4">
               <Icon type="picture" />
               <span>Photo Collections</span>
             </Menu.Item>
-            <Menu.Item key="4">
+            <Menu.Item key="5">
               <Icon type="heart-o" />
               <span>Favorites</span>
             </Menu.Item>
           </Menu>
         </Sider>
         <Layout>
-          <Header style={{ background: '#fff', padding: 0 }}>
-            <Icon
-              className="trigger"
-              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-              onClick={this.toggle}
-            />
-          </Header>
           <Content style={{ background: '#fff', padding: 5 }}>
             <FamiliesContainer />
           </Content>
