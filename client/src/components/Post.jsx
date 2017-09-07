f80b237e91bc380a9aaae0e0a9713d65cbbef345
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-06 22:46:02 -0700

    Add Card for each post and render image, title and description

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -4,7 +4,13 @@ import '../styles.css';
 
 const Post = (props) => (
   <div>
-    
+    <Card style={{ width: 300}} bodyStyle={{ padding: 15 }}>
+      <div className="custom-image" style={{backgroundImage: `url(${props.post.image})`}}></div>
+      <div className="custom-card">
+        <h5>{props.post.title}</h5>
+        <p>{props.post.description}</p>
+      </div>
+    </Card>
   </div>
 );
 
