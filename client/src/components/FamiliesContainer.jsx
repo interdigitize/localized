a90289ff9ad628cc076741b89cffb68c3b86ac76
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-07 14:50:14 -0700

    Remove unnecessary import

--- client/src/components/FamiliesContainer.jsx
+++ client/src/components/FamiliesContainer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Family from './Family.jsx';
 import AddFamily from './AddFamily.jsx';
-import family from '../dummyfamilydata';
 
 const FamiliesContainer = (props) => (
   <div style = {{padding: '20px'}}>
