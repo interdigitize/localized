ad307bc2001ae9233d81e8c5601595da35cf687e
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-06 23:02:31 -0700

    Create component and add in icon to add a family

---
+++ client/src/components/AddFamily.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Icon } from 'antd';
+
+const AddFamily = (props) => (
+  <div>
+    <Icon type="plus-circle-o" />
+    Add a Family
+  </div>
+);
+
+export default AddFamily;
