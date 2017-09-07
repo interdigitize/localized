933ab1cf8d917a23d420de50312c4748377f8e89
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-06 23:03:20 -0700

    Flesh out component and add in AddFamily component

--- client/src/components/FamiliesContainer.jsx
+++ client/src/components/FamiliesContainer.jsx
@@ -3,7 +3,12 @@ import Family from './Family.jsx';
 import AddFamily from './AddFamily.jsx';
 
 const FamiliesContainer = (props) => (
-  
+  <div>
+    <div><Family /></div>
+    <div style= {{float: 'right', padding: '20px'}}>
+      <AddFamily />
+    </div>
+  </div>
 );
 
 export default FamiliesContainer;
