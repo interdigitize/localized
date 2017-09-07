6dbca5fa4400b8aea3452cc6f62ad07daf101486
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-07 14:35:14 -0700

    Add div to render each family member image

--- client/src/components/Family.jsx
+++ client/src/components/Family.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const Family = (props) => (
-  <div>
-    My Family
-  </div>
+  <div className="family-member-image" style={{backgroundImage: `url(${props.member.image})`}}></div>
 );
 
 export default Family;
