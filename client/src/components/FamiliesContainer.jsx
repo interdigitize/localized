0cca60eaccd47b1ba384b9499cf450b828f1e992
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-06 23:01:24 -0700

    Add skeleton in for FamiliesContainer

---
+++ client/src/components/FamiliesContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Family from './Family.jsx';
+import AddFamily from './AddFamily.jsx';
+
+const FamiliesContainer = (props) => (
+  
+);
+
+export default FamiliesContainer;
