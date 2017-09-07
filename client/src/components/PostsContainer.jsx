c2919882e0ca8290d45c37caef9b9c9e7378ba3e
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-06 22:42:05 -0700

    Flesh out component

---
+++ client/src/components/PostsContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Post from './Post.jsx';
+import { Row, Col } from 'antd';
+
+const PostsContainer = (props) => (
+  <div>
+  The Vault
+    <div>
+    
+    </div>
+  </div>
+);
+
+export default PostsContainer;
