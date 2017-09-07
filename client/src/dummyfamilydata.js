8190025210bfdfe38b1f2bc1175cbba63254fa43
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-07 14:38:05 -0700

    Update dummy family data with more images

--- client/src/dummyfamilydata.js
+++ client/src/dummyfamilydata.js
@@ -5,7 +5,10 @@ const family = [
   { 'image': 'https://vignette3.wikia.nocookie.net/xmenmovies/images/6/6b/Hugh-Jackman.jpg/revision/latest?cb=20120222054319'},
   { 'image': 'http://akns-images.eonline.com/eol_images/Entire_Site/2017527/rs_600x600-170627125434-600.Scarlett-Johansson-Hair.jl.062717.jpg?downsize=300:*&crop=300:300;left,top'},
   { 'image': 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/06/15/09/jon-snow.jpg'},
-  { 'image': 'http://michael-fassbender-online.net/wp-content/themes/awake/lib/scripts/timthumb/thumb.php?src=http://michael-fassbender-online.net/wp-content/uploads/2015/08/MF-Tux.jpg&w=980&h=420&zc=1&q=100'}
+  { 'image': 'http://michael-fassbender-online.net/wp-content/themes/awake/lib/scripts/timthumb/thumb.php?src=http://michael-fassbender-online.net/wp-content/uploads/2015/08/MF-Tux.jpg&w=980&h=420&zc=1&q=100'},
+  { 'image': 'http://www.star2.com/wp-content/uploads/2016/03/harry_shum_jr_a_p-e1431739786837-770x470.jpg'},
+  { 'image': 'https://upload.wikimedia.org/wikipedia/commons/9/99/Kate_Winslet_at_The_Dressmaker_event_TIFF_%28headshot%29.jpg'},
+  { 'image': 'http://www.trbimg.com/img-586555ff/turbine/la-en-st-1229-issa-rae-20161228'}
 ];
 
 export default family;
