7238ac5c4e8c730a37a35086c4ad9fd16d29973d
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-06 22:36:21 -0700

    Import necessary components

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,3 +1,7 @@
 import React from 'react';
 import { Button } from 'antd';
-import { Menu, Icon } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
+const { Header, Sider, Content } = Layout;
+import '../styles.css';
+import FamiliesContainer from './FamiliesContainer.jsx';
+import PostsContainer from './PostsContainer.jsx';
