74627329082914d56c8abf5532378ce4f338d9cf
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-06 22:33:54 -0700

    Remove current nav bar: will update nav bar UI

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -3,20 +3,3 @@ import { Button } from 'antd';
 import { Menu, Icon } from 'antd';
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
-
-const Navigation = (props) => (
-  <Menu
-    mode="horizontal">
-    <Menu.Item key="mail">
-      <Icon type="home" />Localized
-    </Menu.Item>
-    <SubMenu title={<span><Icon type="setting" />Navigation Three - Submenu</span>}>
-      <MenuItemGroup title="Item 1">
-        <Menu.Item key="setting:1">Option 1</Menu.Item>
-        <Menu.Item key="setting:2">Option 2</Menu.Item>
-      </MenuItemGroup>
-    </SubMenu>
-  </Menu>
-);
-
-export default Navigation;
