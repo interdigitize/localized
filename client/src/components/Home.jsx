964e06302b6bfa4f321d9d92850a535944e1b8ab
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-07 15:51:02 -0700

    Import dummy data and pass down props

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,19 +4,19 @@ import PostsContainer from './PostsContainer.jsx';
 import UploadMedia from './UploadMedia.jsx';
 import { Layout } from 'antd';
 const { Header, Sider, Content } = Layout;
-
-import dummydata from '../dummydata';
+import posts from '../dummyphotodata';
+import family from '../dummyfamilydata';
 
 const Home = (props) => (
   <Layout style={{flexDirection: 'column'}}>
     <Content style={{ background: '#fff', padding: 5 }}>
-      <FamiliesContainer />
+      <FamiliesContainer familyImages={family} />
     </Content>
     <Content style={{ background: '#f9f9f9', padding: 10 }}>
       <UploadMedia />
     </Content>
     <Content style={{background: '#f1f1f1', padding: 5, minHeight: 800}}>
-      <PostsContainer posts={dummydata.posts}/>
+      <PostsContainer posts={posts}/>
     </Content>
   </Layout>
 );
