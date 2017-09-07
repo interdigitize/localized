38f20503ba15f6b78339861d09eaf6fbd3b5e0d6
cmehrabian <cameron.mehrabian@gmail.com>
2017-09-07 14:54:26 -0700

    added routes and modified 404 page

--- client/src/components/404.jsx
+++ client/src/components/404.jsx
@@ -6,5 +6,4 @@ const NotFound = (props) => (
   </div>
 );
 
-
 export default NotFound;
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Home from './Home.jsx';
+import NotFound from './404.jsx';
+import { Switch, Route } from 'react-router-dom';
 
 const Main = (props) => (
-  <div>
-    <Home />
-  </div>
+  <main>
+    <Switch>
+      <Route exact path='/' render={() => <Home  />} />
+      <Route render={() => <NotFound />} />
+    </Switch>
+  </main>
 );
 
 export default Main;
