0baee5e6ad60ee271f313c8647f7464976e9a6f3
interdigitize <interdigitize@users.noreply.github.com>
2017-09-08 16:30:00 -0700

    Adds Multer middleware to parse media files

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(middleware.morgan('dev'));
 app.use(middleware.cookieParser());
-app.use(middleware.bodyParser.urlencoded({extended: false}));
+app.use(middleware.bodyParser.urlencoded({extended: true}));
 app.use(middleware.bodyParser.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -20,10 +20,13 @@ app.use(middleware.flash());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+let upload = middleware.multer();
+app.use(upload.any());
+
 app.use('/', routes.auth);
 app.use('/api', routes.api);
 app.use('/api/profiles', routes.profiles);
-app.use('/upload', routes.upload);
+app.use('/api/upload', routes.upload);
 
 app.get('*', (req, res) => {
   const preloadedState = {};
