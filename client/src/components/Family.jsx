9722a7be07f3ff89a68e42742d4c7d2492a41f23
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-08 12:46:40 -0700

    Update styling

--- client/src/components/Family.jsx
+++ client/src/components/Family.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import dummydata from '../dummydata';
+import '../styles.css';
 
 const Family = (props) => (
   <div>
-    <div className="family-member-image" style={{display: 'flex', backgroundImage: `url(${props.member.avatar})`}}></div>
+    <div className="family-member-image" style={{backgroundImage: `url(${props.member.avatar})`}}></div>
     <div className="family-member-name">{props.member.first}</div>
   </div>
 );
