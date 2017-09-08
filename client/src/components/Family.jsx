2a1a734662f5699e221f1eb9c9fb1b820b33a99a
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-08 11:58:58 -0700

    Add member name

--- client/src/components/Family.jsx
+++ client/src/components/Family.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import dummydata from '../dummydata';
 
 const Family = (props) => (
-  <div className="family-member-image" style={{backgroundImage: `url(${props.member.image})`}}></div>
+  <div>
+    <div className="family-member-image" style={{display: 'flex', backgroundImage: `url(${props.member.avatar})`}}></div>
+    <div className="family-member-name">{props.member.first}</div>
+  </div>
 );
 
 export default Family;
