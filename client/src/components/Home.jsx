2c41bac6263ea8262b4c7fefad10ac522db05b40
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-08 11:59:36 -0700

    Point to only one dummy data file

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,19 +4,18 @@ import PostsContainer from './PostsContainer.jsx';
 import UploadMedia from './UploadMedia.jsx';
 import { Layout } from 'antd';
 const { Header, Sider, Content } = Layout;
-import posts from '../dummyphotodata';
-import family from '../dummyfamilydata';
+import dummydata from '../dummydata';
 
 const Home = (props) => (
   <Layout style={{flexDirection: 'column'}}>
     <Content style={{ background: '#fff', padding: 5 }}>
-      <FamiliesContainer familyImages={family} />
+      <FamiliesContainer familyImages={dummydata.profiles} />
     </Content>
     <Content style={{ background: '#f9f9f9', padding: 10 }}>
       <UploadMedia />
     </Content>
     <Content style={{background: '#f1f1f1', padding: 5, minHeight: 800}}>
-      <PostsContainer posts={posts}/>
+      <PostsContainer posts={dummydata.posts}/>
     </Content>
   </Layout>
 );
