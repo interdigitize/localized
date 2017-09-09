cc50cafeb2332f13a4bf65903e85932351f437a1
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-09 15:53:39 -0700

    Add functionality to retrieve profiles

--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -6,12 +6,16 @@ const ProfileController = require('../controllers').Profiles;
 router.route('/')
   .get(ProfileController.getAll)
   // .post(ProfileController.create)
-  ;
+;
+
+router.route('/familymembers')
+  .get(ProfileController.getAllFamilyMembers);
 
 router.route('/:id')
   .get(ProfileController.getOne)
   .put(ProfileController.update)
   // .delete(ProfileController.deleteOne)
-  ;
+;
+
 
 module.exports = router;
