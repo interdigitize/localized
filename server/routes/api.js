b5ce1eea67c57d4392dbbca06a739c72b6b5c218
interdigitize <interdigitize@users.noreply.github.com>
2017-09-09 12:18:14 -0700

    Appeases pomander

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Posts = require('../../db/models/posts');
 
 
-router.route('/')
+router.route('/:family_id')
   .get((req, res) => {
     Posts
       .fetchAll()
@@ -15,26 +15,27 @@ router.route('/')
         console.log(error);
       });
     // res.status(200).send('Hello World!');
-  })
-  .post((req, res) => {
-    new Posts({
-      url: req.body.url,
-      user_id: req.body.user_id,
-      family_id: req.body.family_id,
-      type: req.body.type,
-      title: req.body.title,
-      description: req.body.description,
-      etag: req.body.etag//response from AWS
-    })
-      .save()
-      .then((saved) => {
-        res.json({saved});
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    // res.status(201).send({ data: 'Posted!' });
   });
+// .post((req, res) => {
+//   new Posts({
+//     url: req.body.url,
+//     user_id: req.body.user_id,
+//     family_id: req.body.family_id,
+//     type: req.body.type,
+//     title: req.body.title,
+//     description: req.body.description,
+//     etag: req.body.etag//response from AWS
+//   })
+//     .save()
+//     .then((saved) => {
+//       res.json({saved});
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// res.status(201).send({ data: 'Posted!' });
+// });
+
 
 router.route('/:id')
   .put((req, res) => {
