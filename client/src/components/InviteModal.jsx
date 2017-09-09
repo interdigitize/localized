9556714b2317241a1d94638dcd46b467a8567b40
cmehrabian <cameron.mehrabian@gmail.com>
2017-09-08 22:35:35 -0700

    adds inviteModal base

---
+++ client/src/components/InviteModal.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Modal, Button, Form, Input  } from 'antd';
+const FormItem = Form.Item;
+
+class InviteModal extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    <div id='invite-modal'>
+
+    </div>
+  }
+}
+
+export default InviteModal;
