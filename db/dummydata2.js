c4b540696603909c34b5b54428efe3bc6d4bebd8
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-08 17:51:12 -0700

    Add family id to dummy data for posts

--- db/dummydata2.js
+++ db/dummydata2.js
@@ -80,7 +80,8 @@ const dummydataTwo = {
       description: "me",
       type: "jpg",
       created_at: "2017-01-07 18:55:53.201982-05",
-      user_id: 5
+      user_id: 5,
+      family_id: 1
     },
     {
       url: "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg",
@@ -88,7 +89,8 @@ const dummydataTwo = {
       description: "baby",
       type: "jpg",
       created_at: "2017-07-03 18:56:53.201982-05",
-      user_id: 6
+      user_id: 6,
+      family_id: 1
     },
     {
       url: "https://timedotcom.files.wordpress.com/2016/01/time-100-2016-adele-adkins1.jpg?quality=85&w=814",
@@ -96,7 +98,8 @@ const dummydataTwo = {
       description: "adele",
       type: "jpg",
       created_at: "2017-09-03 18:57:59.201982-05",
-      user_id: 7
+      user_id: 7,
+      family_id: 1
     },
     {
       url: "https://vignette3.wikia.nocookie.net/xmenmovies/images/6/6b/Hugh-Jackman.jpg/revision/latest?cb=20120222054319",
@@ -104,7 +107,8 @@ const dummydataTwo = {
       description: "hugh",
       type: "jpg",
       created_at: "2017-09-02 18:58:59.201982-05",
-      user_id: 8
+      user_id: 8,
+      family_id: 1
     },
     {
       url: "http://akns-images.eonline.com/eol_images/Entire_Site/2017527/rs_600x600-170627125434-600.Scarlett-Johansson-Hair.jl.062717.jpg?downsize=300:*&crop=300:300;left,top",
@@ -112,7 +116,8 @@ const dummydataTwo = {
       description: "scarlett",
       type: "jpg",
       created_at: "2017-09-10 18:40:59.201982-05",
-      user_id: 9
+      user_id: 9,
+      family_id: 1
     },
     {
       url: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/06/15/09/jon-snow.jpg",
@@ -120,7 +125,8 @@ const dummydataTwo = {
       description: "jon",
       type: "jpg",
       created_at: "2017-09-06 18:30:59.201982-05",
-      user_id: 10
+      user_id: 10,
+      family_id: 1
     },
     {
       url: "http://michael-fassbender-online.net/wp-content/themes/awake/lib/scripts/timthumb/thumb.php?src=http://michael-fassbender-online.net/wp-content/uploads/2015/08/MF-Tux.jpg&w=980&h=420&zc=1&q=100",
@@ -128,7 +134,8 @@ const dummydataTwo = {
       description: "michael",
       type: "jpg",
       created_at: "2017-10-02 19:40:59.201982-05",
-      user_id: 11
+      user_id: 11,
+      family_id: 1
     },
     {
       url: "http://www.star2.com/wp-content/uploads/2016/03/harry_shum_jr_a_p-e1431739786837-770x470.jpg",
@@ -136,7 +143,8 @@ const dummydataTwo = {
       description: "harry",
       type: "jpg",
       created_at: "2017-03-07 18:40:59.201982-05",
-      user_id: 12
+      user_id: 12,
+      family_id: 1
     },
     {
       url: "https://upload.wikimedia.org/wikipedia/commons/9/99/Kate_Winslet_at_The_Dressmaker_event_TIFF_%28headshot%29.jpg",
@@ -144,7 +152,8 @@ const dummydataTwo = {
       description: "kate",
       type: "jpg",
       created_at: "2017-06-07 18:40:59.201982-05",
-      user_id: 13
+      user_id: 13,
+      family_id: 1
     },
     {
       url: "http://www.trbimg.com/img-586555ff/turbine/la-en-st-1229-issa-rae-20161228",
@@ -152,7 +161,8 @@ const dummydataTwo = {
       description: "issa",
       type: "jpg",
       created_at: "2017-07-07 18:40:59.201982-05",
-      user_id: 14
+      user_id: 14,
+      family_id: 1
     },
     {
       url: "https://i.pinimg.com/736x/e2/e1/c9/e2e1c923f41b3cd650141a68745c434f--eva-mendes-style-prom-hairstyles.jpg",
@@ -160,7 +170,8 @@ const dummydataTwo = {
       description: "eva",
       type: "jpg",
       created_at: "2017-02-07 18:40:59.201982-05",
-      user_id: 15
+      user_id: 15,
+      family_id: 1
     },
     {
       url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Ed_Norton_Shankbone_Metropolitan_Opera_2009.jpg/220px-Ed_Norton_Shankbone_Metropolitan_Opera_2009.jpg",
@@ -168,7 +179,8 @@ const dummydataTwo = {
       description: "ed",
       type: "jpg",
       created_at: "2017-05-07 18:40:59.201982-05",
-      user_id: 16
+      user_id: 16,
+      family_id: 1
     },
     {
       url: "https://media1.s-nbcnews.com/j/newscms/2017_21/1216209/salma-hayek-hair-today-inline-001-170522_243d723cbe725b08ee6f2022670207b5.today-inline-large.jpg",
@@ -176,7 +188,8 @@ const dummydataTwo = {
       description: "selma",
       type: "jpg",
       created_at: "2017-03-07 18:40:59.201982-05",
-      user_id: 17
+      user_id: 17,
+      family_id: 1
     }
   ]
 };
