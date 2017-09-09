b3b8fdd261cc1b5f758f3c1b5c9f88449c29aad6
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-08 17:44:06 -0700

    Add dummy data to posts table in DB

---
+++ db/seeds/3_posts_seed.js
@@ -0,0 +1,23 @@
+const posts = require('../dummydata2.js').dummydataTwo['posts'];
+
+exports.seed = function (knex, Promise) {
+  var postPromises = [];
+  posts.forEach((post) => {
+    postPromises.push(createPosts(knex, post));
+  });
+
+  return Promise.all(postPromises);
+};
+
+const createPosts = (knex, post) => {
+  return knex.table('posts')
+    .insert(
+      {
+        url: post.url,
+        title: post.title,
+        description: post.description,
+        type: post.type,
+        user_id: post.user_id
+      }
+    );
+};
