d83c378535d8f8c761c39a28d44c025812d5153b
cmehrabian <cameron.mehrabian@gmail.com>
2017-09-08 22:31:50 -0700

    adds basic mailer + email template

--- server/controllers/mailer.js
+++ server/controllers/mailer.js
@@ -1,5 +1,43 @@
 const nodemailer = require('nodemailer');
+const mailerConfig = require('config').get('mailer');
+const { emailTemplate } = require('../views/email-template');
 
 module.exports.inviteByEmail = (req, res) => {
+  var { toEmail, fromEmail, fromFirst, fromLast } = req.query;
 
+  var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: mailerConfig.user,
+        clientId: mailerConfig.clientId,
+        clientSecret: mailerConfig.clientSecret,
+        refreshToken: mailerConfig.refreshToken,
+        accessToken: mailerConfig.accessToken
+      }
+    });
+
+    var link = `http://localhost:3000/signup?invitedBy=${fromEmail}&familyid=0`;
+    var email = emailTemplate(link);
+
+    var mailOptions = {
+      from: `${fromFirst} <${fromEmail}>`,
+      to: `${fromEmail}`,
+      subject: `Join your family!`,
+      html: `${email}`
+    };
+
+    var inviteByEmailCB = (bool) => {
+      res.json(bool);
+    }
+
+    transporter.sendMail(mailOptions, (err, res) => {
+      if (err) {
+        console.log(err);
+        inviteByEmailCB(false)
+      } else {
+        console.log('Email Sent');
+        inviteByEmailCB(true);
+      }
+    });
 };
---
+++ server/views/email-template.js
@@ -0,0 +1,103 @@
+module.exports.emailTemplate = function(link) {
+  return (`<body bgcolor="#f6f6f6" style="font-family: Arial; background-color: #f6f6f6;">
+  <table width="630" class="container" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td>
+      <table align="left">
+        <tr>
+          <td width="188" class="Logo">
+            <img src="http://www.rickygipson.com/images/codepen/company_logo.png">
+          </td>
+        </tr>
+      </table>
+      <table align="right">
+        <tr>
+          <td height="70" class="viewWebsite">
+            <p style="font-family: Arial, Helvetica, sans-serif; color: #555555; font-size: 10px; padding: 0; margin: 0;">If you are having trouble viewing this email, please <a href="#" style="color: #990000;">click here</a>.</p>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+  </table>
+
+  <table width="630" bgcolor="#fcfcfc" style="border: 1px solid #dddddd; line-height: 135%;" class="container" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td bgcolor="#fcfcfc" colspan="3" width="100%" height="10">&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#fcfcfc" colspan="3" align="center">
+      <img src="http://www.rickygipson.com/images/codepen/main-image.jpg" width="100%">
+    </td>
+  </tr>
+  <tr>
+    <td colspan="3" height="15">&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#fcfcfc" colspan="3">
+      <table>
+        <tr>
+          <td width="30" class="spacer">&nbsp;</td>
+          <td align="center" class="bodyCopy">
+            <h1 style="font-family: Arial, Helvetica, sans-serif; font-size: 32px; color: #404040; margin-top: 0; margin-bottom: 20px; padding: 0; line-height: 135%" class="headline">You've been invited to your family!</h1>
+            <p style="font-family: Arial, Helvetica, sans-serif; color: #555555; font-size: 14px; padding: 0 40px;">Confused? Dont be! You've been chosen to join a family at Localized.
+              An exclusive opportunity that only the most interesting people get offered. What do you have to do, click the link below and be directed to /signup for Localized. Fill out the remaining information and get access to the family you've always dreamt of.</p>
+            <a href="${link}">Click this link to join your Family</a>
+          </td>
+          <td width="30" class="spacer">&nbsp;</td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+  <tr>
+    <td colspan="3" align="center">
+      <img src="http://www.rickygipson.com/images/codepen/grey_div.jpg" width="95%">
+    </td>
+  </tr>
+  <tr>
+    <td colspan="3" height="3">&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="3" class="force-width">
+      <table width="213" align="left" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;" class="belowConsoles">
+        <tr>
+          <td width="35" class="spacer"></td>
+          <td>
+            <div class="consoleImage">
+            <img src="http://www.rickygipson.com/images/codepen//thumbnails.jpg" class="belowFeatureIMG">
+              </div>
+            <h3 style="font-family: Arial, Helvetica, sans-serif; font-size: 16px; color: #404040; margin: 0; padding: 0;">What is Localized?</h3>
+            <p style="font-family: Arial, Helvetica, sans-serif; color: #555555; font-size: 13px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper aliquet venenatis. Quisque eu nunc vitae elit eleifend accumsan.</p>
+          </td>
+        </tr>
+      </table>
+      <table width="193" align="left" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;" class="belowConsoles">
+        <tr>
+          <td width="15" class="spacer"></td>
+          <td>
+            <div class="consoleImage">
+            <img src="http://www.rickygipson.com/images/codepen/thumbnails.jpg" class="belowFeatureIMG">
+              </div>
+            <h3 style="font-family: Arial, Helvetica, sans-serif; font-size: 16px; color: #404040; margin: 0; padding: 0;">What if I don't want a family?</h3>
+            <p style="font-family: Arial, Helvetica, sans-serif; color: #555555; font-size: 13px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper aliquet venenatis. Quisque eu nunc vitae elit eleifend accumsan.</p>
+          </td>
+        </tr>
+      </table>
+      <table width="193" align="left" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;" class="belowConsoles">
+        <tr>
+          <td width="15" class="spacer"></td>
+          <td>
+            <div class="consoleImage">
+            <img src="http://www.rickygipson.com/images/codepen/thumbnails.jpg" class="belowFeatureIMG">
+              </div>
+            <h3 style="font-family: Arial, Helvetica, sans-serif; font-size: 16px; color: #404040; margin: 0; padding: 0;">What we do with your data</h3>
+            <p style="font-family: Arial, Helvetica, sans-serif; color: #555555; font-size: 13px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper aliquet venenatis. Quisque eu nunc vitae elit eleifend accumsan.</p>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+  </table>
+</body>
+`);
+}
