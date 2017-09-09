2e96c0bf1e118dc7ace776ab12280ecf3c039cc9
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-09 16:05:19 -0700

    Remove unnecessary console logs and old code when Home was stateless

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,34 +1,9 @@
-// import React from 'react';
-// import FamiliesContainer from './FamiliesContainer.jsx';
-// import PostsContainer from './PostsContainer.jsx';
-// import UploadMedia from './UploadMedia.jsx';
-// import { Layout } from 'antd';
-// const { Header, Sider, Content } = Layout;
-// import dummydata from '../dummydata';
-//
-// const Home = (props) => (
-//   <Layout style={{flexDirection: 'column'}}>
-//     <Content style={{ background: '#fff', padding: 5 }}>
-//       <FamiliesContainer familyImages={dummydata.profiles} />
-//     </Content>
-//     <Content style={{ background: '#f9f9f9', padding: 10 }}>
-//       <UploadMedia />
-//     </Content>
-//     <Content style={{background: '#f1f1f1', padding: 5, minHeight: 800}}>
-//       <PostsContainer posts={dummydata.posts}/>
-//     </Content>
-//   </Layout>
-// );
-//
-// export default Home;
-
 import React from 'react';
 import FamiliesContainer from './FamiliesContainer.jsx';
 import PostsContainer from './PostsContainer.jsx';
 import UploadMedia from './UploadMedia.jsx';
 import { Layout } from 'antd';
 const { Header, Sider, Content } = Layout;
-import dummydata from '../dummydata';
 import axios from 'axios';
 
 class Home extends React.Component {
@@ -50,12 +25,10 @@ class Home extends React.Component {
         family_id: this.state.family_id
       }})
       .then((response) => {
-        console.log('assets response is', response.data.contacts);
         if (response.data.contacts) {
           this.setState({
             posts: response.data.contacts
           });
-          console.log('this.posts is', this.state.posts);
         }
       })
       .catch((error) => {
