43b65e5c685c6a729ae122eb74e4273d896e0dd7
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-09 17:22:26 -0700

    Update postsByFamily route

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,40 +3,6 @@ const express = require('express');
 const router = express.Router();
 const Posts = require('../../db/models/posts');
 
-router.route('/:family_id')
-  .get((req, res) => {
-    //need to update so it only returns posts from a specific family_id
-    Posts
-      .fetchAll()
-      .then((contacts) => {
-        res.json({contacts});
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  });
-// res.status(200).send('Hello World!');
-// .post((req, res) => {
-//   new Posts({
-//     url: req.body.url,
-//     user_id: req.body.user_id,
-//     family_id: req.body.family_id,
-//     type: req.body.type,
-//     title: req.body.title,
-//     description: req.body.description,
-//     etag: req.body.etag//response from AWS
-//   })
-//     .save()
-//     .then((saved) => {
-//       res.json({saved});
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// res.status(201).send({ data: 'Posted!' });
-// });
-
-
 router.route('/postsByFamily')
   .get((req, res) => {
     Posts
