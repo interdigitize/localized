fa735b44a7a0d5214f6f786d747035a656cd84d7
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-12 16:44:55 -0700

    Export posts

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,3 +3,4 @@ module.exports.auth = require('./auth');
 module.exports.profiles = require('./profiles');
 module.exports.upload = require('./upload');
 module.exports.mailer = require('./mailer');
+module.exports.posts = require('./posts');
