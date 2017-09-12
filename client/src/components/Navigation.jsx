822ffc8fc4f38aea1374b194af1007bdd53b1559
cmehrabian <cameron.mehrabian@gmail.com>
2017-09-12 16:24:03 -0700

    styling infrastructure updates

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Button } from 'antd';
 import { Layout, Menu, Icon } from 'antd';
 const { Header, Sider, Content } = Layout;
-import '../styles.css';
 import FamiliesContainer from './FamiliesContainer.jsx';
 import PostsContainer from './PostsContainer.jsx';
 import UploadMedia from './UploadMedia.jsx';
