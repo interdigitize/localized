5eba1553733aadc15a64afa7d323803ac44c96b2
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-12 15:31:09 -0700

    Add in ability to edit title and description divs and event listeners for user modifying fields

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -6,8 +6,8 @@ const Post = (props) => (
     <Card style={{ width: 300}} bodyStyle={{ padding: 15 }}>
       <div className="custom-image" style={{backgroundImage: `url(${props.post.url})`}}></div>
       <div className="custom-card">
-        <h5>{props.post.title}</h5>
-        <p>{props.post.description}</p>
+        <div contentEditable={true} onInput={props.savePostTitle}><h5>{props.post.title}</h5></div>
+        <div contentEditable={true} onInput={props.savePostDescription}><p>{props.post.description}</p></div>
       </div>
     </Card>
   </div>
