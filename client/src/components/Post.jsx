d3bd4432b1e4666a8bd0fb4905d9ddeb04d3b6c5
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-13 11:09:50 -0700

    Disable console warning about contentEditable and retrieve user_id of post

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Card } from 'antd';
 
-const Post = (props) => (
-  <div>
-    <Card style={{ width: 300}} bodyStyle={{ padding: 15 }}>
-      <div className="custom-image" style={{backgroundImage: `url(${props.post.url})`}}></div>
-      <div className="custom-card">
-        <div contentEditable={true} id={props.post.id} onInput={props.updatePostTitle}><h5>{props.post.title}</h5></div>
-        <div contentEditable={true} id={props.post.id} onInput={props.updatePostDescription}><p>{props.post.description}</p></div>
-      </div>
-    </Card>
-  </div>
-);
+const Post = (props) => {
+  const poster = props.post.user_id;
+  console.log('poster is', poster);
+  console.error = (() => {
+    const error = console.error;
+    return (exception) => {
+      if ((exception + '').indexOf('Warning: A component is `contentEditable`') !== 0) {
+        error.apply(console, arguments);
+      }
+    };
+  })();
+
+  return (
+    <div>
+      <Card style={{ width: 300}} bodyStyle={{ padding: 15 }}>
+        <div className="custom-image" style={{backgroundImage: `url(${props.post.url})`}}></div>
+        <div className="custom-card">
+          <div contentEditable={true} id={props.post.id} onInput={props.updatePostTitle}><h5>{props.post.title}</h5></div>
+          <div contentEditable={true} id={props.post.id} onInput={props.updatePostDescription}><p>{props.post.description}</p></div>
+        </div>
+      </Card>
+    </div>
+  );
+};
 
 export default Post;
