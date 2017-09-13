f3723d45054912a311d0e6115275d160f1588405
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-12 17:55:00 -0700

    Dynamically update a post based on parameters passed in

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -6,7 +6,7 @@ module.exports.update = (req, res) => {
       if (!post) {
         throw post;
       }
-      post.set(req.body.params.type, req.body.params.title);
+      post.set(req.body.params.type, req.body.params[req.body.params.type]);
       return post.save();
     })
     .then(() => {
@@ -19,74 +19,3 @@ module.exports.update = (req, res) => {
       res.sendStatus(404);
     });
 };
-// module.exports.getAll = (req, res) => {
-//   models.Posts.fetchAll()
-//     .then(posts => {
-//       res.status(200).send(profiles);
-//     })
-//     .catch(err => {
-//       // This code indicates an outside service (the database) did not respond in time
-//       res.status(503).send(err);
-//     });
-// };
-
-// module.exports.getAllByFamilyID = (req, res) => {
-//   models.Posts.fetchAll()
-//   //For now (Sept 12): fetching all profiles b/c we are not pulling in members by family ID yet. We need to add in criterion of retrieving members by family ID.
-//     .then(posts => {
-//       res.status(200).send(posts);
-//     })
-//     .catch(err => {
-//       res.status(503).send(err);
-//     });
-// };
-
-// module.exports.create = (req, res) => {
-//   models.Posts.forge({ username: req.body.username, password: req.body.password })
-//     .save()
-//     .then(result => {
-//       res.status(201).send(result.omit('password'));
-//     })
-//     .catch(err => {
-//       if (err.constraint === 'users_username_unique') {
-//         return res.status(403);
-//       }
-//       res.status(500).send(err);
-//     });
-// };
-
-// module.exports.getOne = (req, res) => {
-//   models.Posts.where({ id: req.params.id }).fetch()
-//     .then(post => {
-//       if (!post) {
-//         throw post;
-//       }
-//       res.status(200).send(post);
-//     })
-//     .error(err => {
-//       res.status(500).send(err);
-//     })
-//     .catch(() => {
-//       res.sendStatus(404);
-//     });
-// };
-
-
-// module.exports.deleteOne = (req, res) => {
-//   models.Posts.where({ id: req.params.id }).fetch()
-//     .then(post => {
-//       if (!post) {
-//         throw post;
-//       }
-//       return post.destroy();
-//     })
-//     .then(() => {
-//       res.sendStatus(200);
-//     })
-//     .error(err => {
-//       res.status(503).send(err);
-//     })
-//     .catch(() => {
-//       res.sendStatus(404);
-//     });
-// };
