38fcdc48b324d02dc29b3e4844e696fd36f9b202
interdigitize <interdigitize@users.noreply.github.com>
2017-09-12 21:02:30 -0700

    Adds tests for the Post component

---
+++ __test__/Post.spec.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import Post from '../client/src/components/Post.jsx';
+
+const dummydata = [
+  {
+    id: 1,
+    url: "https://s3-us-west-1.amazonaws.com/localized-0001/1505188538887_AgeOfTheGeek_1504911888007+2.mp4",
+    title: "Age of the Geek",
+    description: "A classic Hardison-Eliot exchange.",
+    type: "video/mp4",
+    created_at: "2017-09-11 18:55:53.201982-05",
+    user_id: 4
+  },
+  {
+    id: 2,
+    url: `https://s3-us-west-1.amazonaws.com/localized-0001/1505022937257_Bachs+granddaughter.m4a`,
+    title: "Bach's Grandaughter",
+    description: "A rough of a riff inspired by a Beatles song that was inspired by a Bach song",
+    type: "audio/x-m4a",
+    created_at: "2017-09-03 18:56:53.201982-05",
+    user_id: 5
+  }
+];
+
+// const noTypeOrURL = [{
+//   id: 1,
+//   title: "Age of the Geek",
+//   description: "A classic Hardison-Eliot exchange.",
+//   created_at: "2017-09-11 18:55:53.201982-05",
+//   user_id: 4
+// }]
+
+// suggestions use 'in' ??
+
+//Cases to Cover
+describe('Post Component', () => {
+  //1) no props
+  it('It should have props', function(){
+    expect(shallow(<Post post={dummydata[0]}/>).find('[post]')).toBeTruthy();
+  });
+  //2) no post url
+  it('A post should have a url', function(){
+    expect(shallow(<Post post={dummydata[0]}/>).find('[post="https://s3-us-west-1.amazonaws.com/localized-0001/1505188538887_AgeOfTheGeek_1504911888007+2.mp4"]')).toBeTruthy();
+    // expect(shallow(<Post post={noTypeOrURL[0]}/>).find('[post='undefined']')).toBeUndefined();
+  });
+  it('A post should have a type', function(){
+    expect(shallow(<Post post={dummydata[0]}/>).find('[post="video/mp4"]')).toBeTruthy();
+    // expect(shallow(<Post post={noTypeOrURL[0]}/>).find('[post].type')).toBeUndefined();
+  });
+  //3) if the type is audio
+  it('It should render an audio html5 element if the file has an audio mimetype', function(){
+    const wrapper = shallow(<Post post={dummydata[1]}/>);
+    expect(wrapper.contains(
+      <audio className="custom-image" controls>
+        <source src='https://s3-us-west-1.amazonaws.com/localized-0001/1505022937257_Bachs+granddaughter.m4a' type='audio/x-m4a'/>
+      </audio>
+    )).toEqual(true);
+  });
+  //4) if the type is video
+  it('It should render an video html5 element if the file has an video mimetype', function(){
+    const wrapper = shallow(<Post post={dummydata[0]}/>);
+    expect(wrapper.contains(
+      <video className="custom-image" controls>
+        <source src='https://s3-us-west-1.amazonaws.com/localized-0001/1505188538887_AgeOfTheGeek_1504911888007+2.mp4' type='video/mp4'/>
+      </video>
+    )).toEqual(true);
+  });
+
+});
