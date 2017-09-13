705aadf7cea7cd127d2f1779c8afb362cc567702
clarissachin <29084201+clarissachin@users.noreply.github.com>
2017-09-12 21:17:03 -0700

    Add more dummy data so userIDs are in alignment with posts and profiles

---
+++ db/dummydata.js
@@ -0,0 +1,253 @@
+const dummydata = {
+  profiles: [
+    {
+      first: "Cameron",
+      last: "Mehrabian",
+      display: "cmehrabian",
+      email: "cameron.mehrabian+localized@gmail.com",
+      phone: "4157441234",
+      avatar: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAgxAAAAJDg3N2UwOTkzLWM4MDAtNDQ3Yi04YjNjLWVmODQwYmM1NmUwZg.jpg"
+    },
+    {
+      first: "Dad",
+      last: "",
+      display: "dad",
+      email: "cameron.mehrabian+dad@gmail.com",
+      phone: "4157442345",
+      avatar: "https://accrualnet.cancer.gov/sites/accrualnet.cancer.gov/themes/accrualnet/accrualnet-internals/images/avatars/male/Blue.png"
+    },
+    {
+      first: "Mom",
+      last: "",
+      display: "mom",
+      email: "cameron.mehrabian+mom@gmail.com",
+      phone: "4157443456",
+      avatar: "https://accrualnet.cancer.gov/sites/accrualnet.cancer.gov/themes/accrualnet/accrualnet-internals/images/avatars/male/Blue.png"
+    },
+    {
+      first: "Arya",
+      last: "Stark",
+      display: "Arya",
+      email: "aryastark@gmail.com",
+      phone: "4157443986",
+      avatar: "http://static.dnaindia.com/sites/default/files/2016/05/22/462997-arya-stark-in-game-of-thrones-season-6-epiosde-5-the-door.jpg"
+    },
+    {
+      first: "Samuel L.",
+      last: "Jackson",
+      display: "Samuel L.",
+      email: "samuelljackson@gmail.com",
+      phone: "415742386",
+      avatar: "http://images.indianexpress.com/2016/02/samuelljackson-pulpfiction759.jpg"
+    },
+    {
+      first: "Adele",
+      last: "Atkins",
+      display: "Adele",
+      email: "adeleatkins@gmail.com",
+      phone: "4177903986",
+      avatar: "https://timedotcom.files.wordpress.com/2016/01/time-100-2016-adele-adkins1.jpg?quality=85&w=814"
+    },
+    {
+      first: "Hugh",
+      last: "Jackman",
+      display: "Hugh",
+      email: "hughjackman@gmail.com",
+      phone: "9257443986",
+      avatar: "https://vignette3.wikia.nocookie.net/xmenmovies/images/6/6b/Hugh-Jackman.jpg/revision/latest?cb=20120222054319"
+    },
+    {
+      first: "Scarlett",
+      last: "Johansson",
+      display: "Scarlett",
+      email: "scarlettj@gmail.com",
+      phone: "8257443986",
+      avatar: "http://akns-images.eonline.com/eol_images/Entire_Site/2017527/rs_600x600-170627125434-600.Scarlett-Johansson-Hair.jl.062717.jpg?downsize=300:*&crop=300:300;left,top"
+    },
+    {
+      first: "Jon",
+      last: "Snow",
+      display: "Jon",
+      email: "jonsnow@gmail.com",
+      phone: "4407043086",
+      avatar: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/06/15/09/jon-snow.jpg"
+    },
+    {
+      first: "Michael",
+      last: "Fassbender",
+      display: "Michael",
+      email: "mfassbender@gmail.com",
+      phone: "9234801312",
+      avatar: "http://michael-fassbender-online.net/wp-content/themes/awake/lib/scripts/timthumb/thumb.php?src=http://michael-fassbender-online.net/wp-content/uploads/2015/08/MF-Tux.jpg&w=980&h=420&zc=1&q=100"
+    },
+    {
+      first: "Harry",
+      last: "Shum Jr",
+      display: "Harry",
+      email: "harryshumjr@gmail.com",
+      phone: "9234808342",
+      avatar: "http://www.star2.com/wp-content/uploads/2016/03/harry_shum_jr_a_p-e1431739786837-770x470.jpg"
+    },
+    {
+      first: "Kate",
+      last: "Winslet",
+      display: "Kate",
+      email: "katewinslet@gmail.com",
+      phone: "2334808342",
+      avatar: "https://upload.wikimedia.org/wikipedia/commons/9/99/Kate_Winslet_at_The_Dressmaker_event_TIFF_%28headshot%29.jpg"
+    },
+    {
+      first: "Issa",
+      last: "Rae",
+      display: "Issa",
+      email: "issarae@gmail.com",
+      phone: "2334808342",
+      avatar: "http://www.trbimg.com/img-586555ff/turbine/la-en-st-1229-issa-rae-20161228"
+    },
+    {
+      first: "Eva",
+      last: "Mendes",
+      display: "Eva",
+      email: "evamendes@gmail.com",
+      phone: "1334308442",
+      avatar: "https://i.pinimg.com/736x/e2/e1/c9/e2e1c923f41b3cd650141a68745c434f--eva-mendes-style-prom-hairstyles.jpg"
+    },
+    {
+      first: "Ed",
+      last: "Norton",
+      display: "Ed",
+      email: "ednorton@gmail.com",
+      phone: "6334309432",
+      avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Ed_Norton_Shankbone_Metropolitan_Opera_2009.jpg/220px-Ed_Norton_Shankbone_Metropolitan_Opera_2009.jpg"
+    },
+    {
+      first: "Selma",
+      last: "Hayek",
+      display: "Selma",
+      email: "selmahayek@gmail.com",
+      phone: "6334381433",
+      avatar: "https://media1.s-nbcnews.com/j/newscms/2017_21/1216209/salma-hayek-hair-today-inline-001-170522_243d723cbe725b08ee6f2022670207b5.today-inline-large.jpg"
+    }
+  ],
+  posts: [
+    {
+      url: "http://static.dnaindia.com/sites/default/files/2016/05/22/462997-arya-stark-in-game-of-thrones-season-6-epiosde-5-the-door.jpg",
+      title: "arya",
+      description: "me",
+      type: "jpg",
+      created_at: "2017-01-07 18:55:53.201982-05",
+      user_id: 5,
+      family_id: 1
+    },
+    {
+      url: "http://images.indianexpress.com/2016/02/samuelljackson-pulpfiction759.jpg",
+      title: "godfather",
+      description: "samuel l.",
+      type: "jpg",
+      created_at: "2017-07-03 18:56:53.201982-05",
+      user_id: 6,
+      family_id: 1
+    },
+    {
+      url: "https://timedotcom.files.wordpress.com/2016/01/time-100-2016-adele-adkins1.jpg?quality=85&w=814",
+      title: "godmother",
+      description: "adele",
+      type: "jpg",
+      created_at: "2017-09-03 18:57:59.201982-05",
+      user_id: 7,
+      family_id: 1
+    },
+    {
+      url: "https://vignette3.wikia.nocookie.net/xmenmovies/images/6/6b/Hugh-Jackman.jpg/revision/latest?cb=20120222054319",
+      title: "brother in law",
+      description: "hugh",
+      type: "jpg",
+      created_at: "2017-09-02 18:58:59.201982-05",
+      user_id: 8,
+      family_id: 1
+    },
+    {
+      url: "http://akns-images.eonline.com/eol_images/Entire_Site/2017527/rs_600x600-170627125434-600.Scarlett-Johansson-Hair.jl.062717.jpg?downsize=300:*&crop=300:300;left,top",
+      title: "aunt",
+      description: "scarlett",
+      type: "jpg",
+      created_at: "2017-09-10 18:40:59.201982-05",
+      user_id: 9,
+      family_id: 1
+    },
+    {
+      url: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/06/15/09/jon-snow.jpg",
+      title: "cousin",
+      description: "jon",
+      type: "jpg",
+      created_at: "2017-09-06 18:30:59.201982-05",
+      user_id: 10,
+      family_id: 1
+    },
+    {
+      url: "http://michael-fassbender-online.net/wp-content/themes/awake/lib/scripts/timthumb/thumb.php?src=http://michael-fassbender-online.net/wp-content/uploads/2015/08/MF-Tux.jpg&w=980&h=420&zc=1&q=100",
+      title: "uncle",
+      description: "michael",
+      type: "jpg",
+      created_at: "2017-10-02 19:40:59.201982-05",
+      user_id: 11,
+      family_id: 1
+    },
+    {
+      url: "http://www.star2.com/wp-content/uploads/2016/03/harry_shum_jr_a_p-e1431739786837-770x470.jpg",
+      title: "brother",
+      description: "harry",
+      type: "jpg",
+      created_at: "2017-03-07 18:40:59.201982-05",
+      user_id: 12,
+      family_id: 1
+    },
+    {
+      url: "https://upload.wikimedia.org/wikipedia/commons/9/99/Kate_Winslet_at_The_Dressmaker_event_TIFF_%28headshot%29.jpg",
+      title: "stepsister",
+      description: "kate",
+      type: "jpg",
+      created_at: "2017-06-07 18:40:59.201982-05",
+      user_id: 13,
+      family_id: 1
+    },
+    {
+      url: "http://www.trbimg.com/img-586555ff/turbine/la-en-st-1229-issa-rae-20161228",
+      title: "sister",
+      description: "issa",
+      type: "jpg",
+      created_at: "2017-07-07 18:40:59.201982-05",
+      user_id: 14,
+      family_id: 1
+    },
+    {
+      url: "https://i.pinimg.com/736x/e2/e1/c9/e2e1c923f41b3cd650141a68745c434f--eva-mendes-style-prom-hairstyles.jpg",
+      title: "wife",
+      description: "eva",
+      type: "jpg",
+      created_at: "2017-02-07 18:40:59.201982-05",
+      user_id: 15,
+      family_id: 2
+    },
+    {
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Ed_Norton_Shankbone_Metropolitan_Opera_2009.jpg/220px-Ed_Norton_Shankbone_Metropolitan_Opera_2009.jpg",
+      title: "brother",
+      description: "ed",
+      type: "jpg",
+      created_at: "2017-05-07 18:40:59.201982-05",
+      user_id: 16,
+      family_id: 2
+    },
+    {
+      url: "https://media1.s-nbcnews.com/j/newscms/2017_21/1216209/salma-hayek-hair-today-inline-001-170522_243d723cbe725b08ee6f2022670207b5.today-inline-large.jpg",
+      title: "sister",
+      description: "selma",
+      type: "jpg",
+      created_at: "2017-03-07 18:40:59.201982-05",
+      user_id: 17,
+      family_id: 2
+    }
+  ]
+};
+
+module.exports.dummydata = dummydata;
